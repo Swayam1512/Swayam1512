b1c0836afe089511b04092ef09cb35c88c794ea2
Swayamgadhvana <161429303+Swayamgadhvana@users.noreply.github.com>
2024-03-23 10:37:26 +0530

    Add files via upload

---
+++ class and object.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+#include<string>
+using namespace std;
+class car
+{
+	public:
+		
+		string brand;
+		string model;
+		string price;
+		
+		void dispoutput()
+		{
+			cout<<"BRAND : "<<brand<<endl;
+			cout<<"MODEL : "<<model<<endl;
+			cout<<"PRICE : "<<price<<endl;
+		}
+};
+	int main()
+	{
+		car c1;
+		c1.brand="toyota";
+		c1.model="toyota";
+		c1.price="80";	
+		c1.dispoutput();
+		return 0;
+	}
+
